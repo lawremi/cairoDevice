327005022fcdb787021d704ba890f1e9d6c172df
lawremi <lawremi@cc50f0b1-a3fe-0310-a41b-dd7a1cad1176>
2006-03-14 23:02:16 +0000

    No longer try to use RGtk2.

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,8 @@
 .First.lib <- function(libname, pkgname)
 {
     library.dynam("cairoDevice", pkgname, libname)
-    # We prefer RGtk2, but we can fallback to basic event handling
-	# using an input handler (on linux) - on windows we will use tcl_do hack
-	if (!require("RGtk2"))
-	{
 		.C("loadGTK", PACKAGE="cairoDevice")
 		.C("R_gtk_setEventHandler", PACKAGE="cairoDevice")
-	}
 }
 
 .Last.lib <- function(libname, pkgname)
